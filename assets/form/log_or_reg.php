3d5d34cbf0c2aa7eb954a9beb268824d057b7d5b
Vincent <102018577+Chrinios@users.noreply.github.com>
2022-10-04 16:25:04 +0200

    0.4
    Add some things

---
+++ assets/form/log_or_reg.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <div>
+            <?php
+                include_once('../dossier_exam/assets/Fh/header.php')
+            ?>
+        </div>
+        <div>
+            <div>
+                <a href="">Log-in</a>
+                <a href="">Register</a>
+            </div>
+            <div></div>
+            <div></div>
+        </div>
+        <div>
+            <?php 
+                include_once('../dossier_exam/assets/Fh/footer.php')
+            ?>
+        </div>
+    </div>
+</body>
+</html>
